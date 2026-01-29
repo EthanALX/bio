f6eec699b4f454c09accd7fc93b2be5ee3b03943
EthanALX <EthanAlex0001@outlook.com>
2026-01-29 18:13:05 +0000

    feat: hover

--- frontend/src/features/activity-dashboard/components/YearSelector/YearSelector.tsx
+++ frontend/src/features/activity-dashboard/components/YearSelector/YearSelector.tsx
@@ -1,30 +1,44 @@
-import React from 'react';
-import { useYearSelector } from './YearSelector.hook';
-import styles from './YearSelector.module.css';
+import React from "react";
+import { useYearSelector } from "./YearSelector.hook";
+import styles from "./YearSelector.module.css";
 
 interface YearSelectorProps {
-    years: number[];
-    selectedYear: number;
-    onYearChange: (year: number) => void;
+  years: number[];
+  selectedYear: number;
+  onYearChange: (year: number) => void;
 }
 
 export function YearSelector(props: YearSelectorProps) {
-    const { actions } = useYearSelector(props);
-    const { handleYearChange } = actions;
+  const { actions } = useYearSelector(props);
+  const { handleYearChange } = actions;
+  console.log("ye", props.selectedYear);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.yearList}>
-                {props.years.map((year) => (
-                    <div
-                        key={year}
-                        className={`${styles.yearBlock} ${year === props.selectedYear ? styles.active : ''}`}
-                        onClick={() => handleYearChange(year)}
-                    >
-                        {year}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.yearList}>
+        {props.years.map((year) => (
+          <div
+            key={year}
+            className={`${styles.yearBlock} ${year === props.selectedYear ? styles.active : ""}`}
+            onClick={() => handleYearChange(year)}
+          >
+            {year
+              .toString()
+              .split("")
+              .map((char, index) => (
+                <span
+                  key={index}
+                  className={styles.yearChar}
+                  style={{
+                    transitionDelay: `${index * 0.05}s`,
+                  }}
+                >
+                  {char}
+                </span>
+              ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
